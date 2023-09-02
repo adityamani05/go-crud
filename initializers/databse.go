1295d3bb9c9fd46bd0dfbe1e7d46f287bfde1ba0
Aditya Mani <aditya@Adityas-MacBook-Air.local>
2023-09-02 21:17:07 +0530

    go crud apis added

---
+++ initializers/databse.go
@@ -0,0 +1,18 @@
+package initializers
+
+import (
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDB() {
+	var err error
+	DB, err = gorm.Open(sqlite.Open("go-crud.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
+}
