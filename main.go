2fb923ed2fd0c83ac045fed48569a327ac503427
Aditya Mani <aditya@Adityas-MacBook-Air.local>
2023-09-02 21:17:07 +0530

    go crud apis added

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"go-crud/controllers"
+	"go-crud/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
+
+func main() {
+	r := gin.Default()
+
+	r.POST("/posts", controllers.PostCreate)
+	r.GET("/posts", controllers.PostIndex)
+	r.GET("/posts/:id", controllers.PostsShow)
+	r.PUT("/posts/:id", controllers.PostsUpdate)
+	r.DELETE("/posts/:id", controllers.PostsDelete)
+
+	r.Run()
+}
